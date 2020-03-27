05271eec89a86370fa003a6b43c394a48189e2e6
Zach Gwirtz <zachgw@gmail.com>
2020-03-26 18:35:51 -0700

    join compnent basics

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import AppContext from './context/app-context.js';
 import { Route } from 'react-router-dom';
 
 import Landing from './components/landing/Landing';
+import Join from './components/join/Join';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        user: "test"
-      }
+    this.state = {
+      user: 'test'
+    };
   }
  
   render() {
@@ -24,9 +25,15 @@ export default class App extends React.Component {
             component={Landing}
           />
 
+          <Route 
+            exact
+            path='/join'
+            component={Join}
+          />
+
         </div>
       </AppContext.Provider>
-    )
+    );
   }
 }
 
