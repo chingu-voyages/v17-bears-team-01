e06a3713b1ae96a5aa7ad68116c37e267f9028ab
Piotr Grzybowski <grzybek6@gmail.com>
2020-03-28 13:26:19 +0000

    Config passport Google auth

--- server/app.js
+++ server/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { ApolloServer, gql } = require('apollo-server-express');
-const { typeDefs } = require("./typeDefs");
-const { resolvers } = require("./resolvers");
+const { typeDefs } = require('./typeDefs');
+const { resolvers } = require('./resolvers');
+const passport = require(passport);
 
 require('dotenv').config();
 
 const port = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Connected to database!"))
-    .catch(err => console.log(err));
+  .then(() => console.log('Connected to database!'))
+  .catch(err => console.log(err));
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
+
+require('./config/passport');
+
 server.applyMiddleware({ app });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
