cf6c7ec356cb5f601ff4ac7225065e98f0bd0957
Piotr Grzybowski <grzybek6@gmail.com>
2020-04-04 12:22:13 +0100

    Add sass loader and settings

--- src/components/Simple/Button/Button.js
+++ src/components/Simple/Button/Button.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import styles from './Button.scss';
 
+// eslint-disable-next-line react/prop-types
+const Button = ({ variant = '', ...otherProps }) => (
+  <button
+    {...otherProps}
+    className={
+      styles.component +
+      variant
+        .split(' ')
+        .map(name => ' ' + (styles[name] || name))
+        .join('')
+    }
+  />
+);
+
+export default Button;
