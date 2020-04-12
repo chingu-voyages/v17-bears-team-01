03f10efa38e0035af42673d26eb9bf3c1cfe7ba4
Deep Patel <deep@Deeps-MacBook-Pro.local>
2020-04-12 13:59:31 -0400

    added dashboard fetch

--- src/components/Views/Dashboard/Dashboard.js
+++ src/components/Views/Dashboard/Dashboard.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
 import { Link } from 'react-router-dom';
+import { Query } from '@apollo/react-components';
+import { gql } from 'apollo-boost';
+
+const GET_MEETINGS = gql`
+    {
+        getMeetings{
+            title
+            author
+            description
+            duration
+            timezone
+            availability
+            participants {
+                user_id
+                intervals
+            }
+        }
+    } 
+`;
 
 export default class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            availableMeetings: ['tuesday', 'wednsday', 'thursday']
+            availableMeetings: ['tuesday', 'wednsday', 'thursday'],
+            meetings: {}
         }
     }
+    getMeetings = () => {
+        return(
+        <Query query={GET_MEETINGS}>
+                {({ loading, error, data }) => {
+                    if (loading) return "Loading...";
+                    if (error) return `Error! ${error.message}`;
+                    this.setState({
+                        meetings: data
+                    });
+                }}
+        </Query>
+        );
+    };
 
     componentDidMount() {
-        console.log('fetching meetings')
+        this.getMeetings();
     }
 
     render() {
