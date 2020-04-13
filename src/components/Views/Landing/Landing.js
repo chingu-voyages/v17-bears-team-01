75d5e06119167e4d12d0eb717dc5c483df3e56cc
TecDvr <zach.gwirtz@datadoghq.com>
2020-04-13 07:56:05 -0700

    update

--- src/components/Views/Landing/Landing.js
+++ src/components/Views/Landing/Landing.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Button from '../../Simple/Button/Button';
 import styles from './Landing.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonClick = this.buttonClick.bind(this);
+    this.state = {
+      loginButton: false
+    }
+  }
+
+  buttonClick() {
+    this.setState({ loginButton: true })
+  }
+
   render() {
     return (
       <div className={styles.component}>
@@ -17,13 +28,20 @@ export default class Landing extends React.Component {
         </div>
         <h3 className={styles.subtitle}>Bringing Peple Together</h3>
         <div className={styles.buttons}>
-          <Link to="/login">
-            <Button>start meeting people</Button>
-          </Link>
-          <p>have an account?</p>
-          <Link to="/login">
-            <p>sign in</p>
-          </Link>
+
+            {this.state.loginButton === false
+            ? <Button onClick={this.buttonClick}>start meeting people</Button>
+            : 
+              <div className='login-container'>
+        <a href="http://localhost:4000/auth/facebook" className="loginBtn loginBtn--facebook">
+        Login with Facebook
+        </a>
+  
+        <a href="http://localhost:4000/auth/google" className="loginBtn loginBtn--google">
+        Login with Google
+        </a>
+      </div>
+            }
         </div>
         {/* <a href="http://localhost:4000/auth/logout">Click to log out</a> */}
       </div>
